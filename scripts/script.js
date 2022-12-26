c1ce1702648f92cc76b9f3935dcd04564aa93ba5
Evgeniya Kostyrya <zhenya241191@gmail.com>
2022-12-26 16:59:51 +0300

    feat: add slider next

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,71 @@
 const fotoTemplate = document.querySelector('#fotoCard').content;
 const fotoCards = document.querySelector('.cards');
+const popups = document.querySelectorAll('.popup');
+const fotoSlider = document.querySelector('#fotoSlider');
 
+//функция открытия попапов
+function openPopup(popup) {
+    popup.classList.add('popup_opened');
+    document.addEventListener('keydown', closeByEscape);
+}
+
+//функция закрытия попапов
+function closePopup(popup) {
+    popup.classList.remove('popup_opened');
+    document.removeEventListener('keydown', closeByEscape);
+  }
+  
+//закрытие попапов на esc
+  
+function closeByEscape(evt) {
+    if (evt.key === 'Escape') {
+      const openedPopup = document.querySelector('.popup_opened');
+      closePopup(openedPopup);
+    }
+}
+
+//закрытие по клику на оверлэй и крестик
+
+popups.forEach(function(popup) {
+    popup.addEventListener('mousedown', function(evt) {
+      if (evt.target.classList.contains('popup')) {
+        closePopup(popup);
+      }
+      if (evt.target.classList.contains('popup__close-button')) {
+       closePopup(popup);
+     }
+    })
+  });
+
+//slider
+
+function sliderNext (cardImages, sliderImage) {
+    let n;
+    for(let i = 0; i < cardImages.length; i++) {
+        if (cardImages[i].src === sliderImage.src) {
+            i === cardImages.length - 1 ? n = 0 : n = i+1
+            sliderImage.src = cardImages[n].src;
+            break;
+        }
+   }
+}
+
+  
 function createFotos(obj) {
     const card = fotoTemplate.querySelector('.card').cloneNode(true);
-    card.querySelector('.card__image').src = obj.src;
-    card.querySelector('.card__image').alt = obj.alt;
-    card.querySelector('.card__name').textContent = obj.name;
-    card.querySelector('.card__prof').textContent = obj.prof;
+    const image = card.querySelector('.card__image');
+    const name = card.querySelector('.card__name');
+    const prof = card.querySelector('.card__prof');
+    image.src = obj.src;
+    image.alt = obj.alt;
+    name.textContent = obj.name;
+    prof.textContent = obj.prof;
+    
+    image.addEventListener('click', function() {
+      openPopup(fotoSlider);
+      fotoSlider.querySelector('.slider__item').src = obj.src;;
+      fotoSlider.querySelector('.slider__item').alt = obj.alt;
+    });
 
     return card;
 }
@@ -21,8 +80,15 @@ const battleCards = document.querySelector('#battleCards');
 
 function createBattles(obj) {
     const card = battleTemplate.querySelector('.card').cloneNode(true);
-    card.querySelector('.card__image').src = obj.src;
-    card.querySelector('.card__image').alt = obj.alt;
+    const image = card.querySelector('.card__image');
+    image.src = obj.src;
+    image.alt = obj.alt;
+
+    image.addEventListener('click', function() {
+        openPopup(fotoSlider);
+        fotoSlider.querySelector('.slider__item').src = obj.src;;
+        fotoSlider.querySelector('.slider__item').alt = obj.alt;
+      });
 
     return card;
 }
@@ -31,3 +97,13 @@ initialBattleCard.forEach(function(obj) {
     const card = createBattles(obj);
     battleCards.append(card);
 });
+
+
+
+const buttonsForvard = fotoSlider.querySelector('.slider__button-next');
+const cardsImages = fotoCards.querySelectorAll('.card__image')
+const sliderImages = fotoSlider.querySelector('.slider__item')
+const cardImages = battleCards.querySelectorAll('.card__image')
+
+buttonsForvard.addEventListener('click',  () => sliderNext(cardsImages, sliderImages))
+buttonsForvard.addEventListener('click',  () => sliderNext(cardImages, sliderImages))
